ae38b1b90a381ec96a85e249b9b443a102aed17d
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2024-08-19 16:13:54 +0530

    gemini completed

--- src/components/ChatEnvironment.jsx
+++ src/components/ChatEnvironment.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import ReactMarkdown from 'react-markdown'
+import axios from 'axios'
+import { GoogleAIFileManager } from '@google/generative-ai/server'
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY)
+const fileManager = new GoogleAIFileManager(import.meta.env.VITE_API_KEY)
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
 export default function ChatEnvironment() {
-  const [input, setInput] = React.useState('')
-  const [data, setData] = React.useState([{ input: '', response: 'Hi there! How can I help you?'}])
+  const [input, setInput] = React.useState('Analyse the image and give me a complaint format to the respective Indian railway department ith prioirtize the seriousnous of the issue shown. Display department of railway, category, priority and the complaint format one by one.')
+  const [file, setFile] = React.useState(null)
+  const [uploadResponse, setUpload] = React.useState(null)
+  const [data, setData] = React.useState([
+    { input: '', response: 'Hi there! How can I help you?' },
+  ])
 
   React.useEffect(() => {
     const storedData = localStorage.getItem('myData')
@@ -15,16 +22,32 @@ export default function ChatEnvironment() {
     }
   }, [])
 
-  async function getResponse() {
+
+  async function getResponse(data) {
     const prompt = input
     setData((prevData) => [
       ...prevData,
       { input: prompt, response: 'Generating response...' },
     ])
 
+
+
     try {
-      const result = await model.generateContent(prompt)
-      const responseText = result.response.candidates[0].content.parts[0].text
+      const uploadedFile = await fileManager.getFile(data.name)
+      console.log('uploadedFile', uploadedFile)
+      const result = await model.generateContent([
+        {
+          text: prompt,
+        },
+        {
+          fileData: {
+            mimeType: uploadedFile.mimeType,
+            fileUri: uploadedFile.uri,
+          },
+        },
+      ])
+      console.log('result', result)
+      const responseText = result.response.text()
 
       setData((prevData) => {
         const newData = [...prevData]
@@ -37,27 +60,53 @@ export default function ChatEnvironment() {
         return newData
       })
     } catch (error) {
-       setData((prevData) => {
-         const newData = [...prevData]
-         const lastIndex = newData.length - 1
-         newData[lastIndex] = {
-           ...newData[lastIndex],
-           response: "**Error generating response, Kindly retry**",
-         }
-         return newData
-       })
+      setData((prevData) => {
+        const newData = [...prevData]
+        const lastIndex = newData.length - 1
+        newData[lastIndex] = {
+          ...newData[lastIndex],
+          response: '**Error generating response, Kindly retry**',
+        }
+        return newData
+      })
+      console.error('Error in generating content', error)
     }
   }
 
+  async function handleFileUpload() {
+    if (!file) {
+      console.log(file)
+
+      console.log('No file selected')
+      return
+    }
+    const formData = new FormData()
+    formData.append('image', file)
+    axios
+      .post('/api/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((res) => {
+        console.log(res.data)
+        setUpload(res.data.data)
+        getResponse(res.data.data)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
   function handleNewChat() {
-    setData([{ input: '', response: 'Hi there! How can I help you?'}])
+    setData([{ input: '', response: 'Hi there! How can I help you?' }])
     localStorage.removeItem('myData')
   }
 
   function handleSubmit(e) {
     e.preventDefault()
     if (input === '') return
-    getResponse()
+    handleFileUpload()
     setInput('')
     setTimeout(() => {
       window.scrollTo({
@@ -72,24 +121,36 @@ export default function ChatEnvironment() {
       <section className="pb-20 pt-14 flex flex-col">
         {data.map((item, index) => (
           <>
-            {item.input && 
-            <section
-              className="user-chat flex flex-col px-5 py-3 self-end"
-              key={index}
-            >
-              <h3 className="font-bold self-end p-2 text-purple-500">You</h3>
-              <p className="bg-gray-800 font-normal px-4 py-3 rounded-md text-gray-200">
-                {item.input}
-              </p>
-            </section>
-            }
+            {item.input && (
+              <section
+                className="user-chat flex flex-col px-5 py-3 self-end"
+                key={index}
+              >
+                <h3 className="font-bold self-end p-2 text-purple-500">You</h3>
+                <p className="bg-gray-800 font-normal px-4 py-3 rounded-md text-gray-200">
+                  {item.input}
+                </p>
+                {uploadResponse && (
+                  <img
+                    src={URL.createObjectURL(file)}
+                    alt="uploaded"
+                    className="w-40 h-40 rounded-md mt-2"
+                  />
+                )}
+              </section>
+            )}
             <section className="ai-reply flex flex-col p-2 self-start">
               <h3 className="font-bold self-start p-1 text-green-500">
-                Gemini
+                Railway Service
               </h3>
 
               <ReactMarkdown
-                className={`font-normal markdown p-3 ${item.response === '**Error generating response, Kindly retry**' ? 'bg-red-600' : 'bg-gray-800' } rounded-md leading-relaxed text-gray-200`}
+                className={`font-normal markdown p-3 ${
+                  item.response ===
+                  '**Error generating response, Kindly retry**'
+                    ? 'bg-red-600'
+                    : 'bg-gray-800'
+                } rounded-md leading-relaxed text-gray-200`}
                 children={item.response}
               />
             </section>
@@ -100,13 +161,17 @@ export default function ChatEnvironment() {
         className="fixed bottom-0 p-4 flex justify-center w-full bg-gray-950 mt-8"
         onSubmit={handleSubmit}
       >
-        <button type="button" className="bg-gray-900 border border-gray-700 rounded-full mr-2 text-xl text-white hover:bg-gray-800 disabled:bg-gray-400" onClick={handleNewChat}>+</button> 
+        <button
+          type="button"
+          className="bg-gray-900 border border-gray-700 rounded-full mr-2 text-xl text-white hover:bg-gray-800 disabled:bg-gray-400"
+          onClick={handleNewChat}
+        >
+          +
+        </button>
         <input
-          type="text"
-          value={input}
+          type="file"
           className="p-2 text-gray-50 rounded-md rounded-r-none w-[30rem] focus:outline-none bg-gray-700"
-          placeholder="Enter your prompt..."
-          onChange={(e) => setInput(e.target.value)}
+          onChange={(e) => setFile(e.target.files[0])}
         />
         <button
           disabled={false}
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['express', 'vite-express', 'multer', './server/gemini.js'],
+  },
+  build: {
+    rollupOptions: {
+      input: './server/index.js',
+    },
+  },
 })
